eeee8f8523346c420933cf4de532de5b53bf1c8f
yihuan.lin <yihuan.lin@yahutech.com>
2024-07-29 15:52:33 +0800

    feat:調整各尺寸牌陣大小

--- ta-project/src/components/card-draw.jsx
+++ ta-project/src/components/card-draw.jsx
@@ -9,10 +9,10 @@ const CardDraw = ({ CardDrawHandler,Cards }) => {
      
       <div className="w-full flex flex-wrap justify-center"  >
         {Cards.map((card) => (
-          !card.hidden ?<CardSingle onClick={() => CardDrawHandler(card.cardId)} key={card.cardId}><CardImg number="TarDefault"></CardImg></CardSingle>:
+          !card.hidden ?<CardSingle className="lg:w-[100px] md:w-[90px] sm:w-[60px] xs:w-[50px]"  onClick={() => CardDrawHandler(card.cardId)} key={card.cardId}><CardImg  number="TarDefault"></CardImg></CardSingle>:
           !card.style
-          ?<CardSingle key={card.cardId}><CardImg className="hidden"  number="TarDefault"></CardImg></CardSingle>:
-          <CardSingle  key={card.cardId}><CardImg className="up-animation"  number="TarDefault"></CardImg></CardSingle>
+          ?<CardSingle className="lg:w-[100px] md:w-[90px] sm:w-[60px] xs:w-[50px]" key={card.cardId}><CardImg className="opacity-0" number="TarDefault"></CardImg></CardSingle>:
+          <CardSingle className="lg:w-[100px] md:w-[90px] sm:w-[60px] xs:w-[50px]"  key={card.cardId}><CardImg className="up-animation"  number="TarDefault"></CardImg></CardSingle>
         ))}
       </div></>
 
--- ta-project/src/components/card-single.jsx
+++ ta-project/src/components/card-single.jsx
@@ -1,7 +1,7 @@
 
 import { twMerge } from 'tailwind-merge'
 const CardSingle = ({children,onClick,className,id}) => {
-  return (<div onClick={onClick} id={id} className={twMerge('lg:w-[120px] md:w-[60px] sm:w-[55px] xs:w-[40px]  bg-black border-2 border-white h-auto rounded-md',className)}>
+  return (<div onClick={onClick} id={id} className={twMerge('lg:w-[120px]  md:w-[90px]  sm:w-[85px] ] xs:w-[80px] bg-black border-2 border-white rounded-md',className)}>
     {children}
   </div>)
 
