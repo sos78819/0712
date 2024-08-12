548e3d117018edbd0df067b4a5697a11d6e93c72
YiHuan <linyc280@gmai.com>
2024-08-12 12:04:51 +0800

    調整按鈕高度

--- ta-project/src/components/card-suffle-button.jsx
+++ ta-project/src/components/card-suffle-button.jsx
@@ -1,5 +1,5 @@
 const CardShuffleButton = ({CardShuffleHandler}) =>{
-  return <div><button className="float-end rounded-md bg-cyan-500 text-slate-800 p-2 my-2 mr-2 hover:bg-cyan-300" onClick={()=>CardShuffleHandler()}>ReStart</button></div>
+  return <div className="flex z-1 h-12 w-full"><button className="rounded-md bg-cyan-500 text-slate-800 p-2 my-2 mr-2 hover:bg-cyan-300 absolute right-0 z-9" onClick={()=>CardShuffleHandler()}>ReStart</button></div>
 }
 
 export { CardShuffleButton }
