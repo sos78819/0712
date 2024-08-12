3abc37a84674a7bed85abd4cd1bb30a5b6c0dd1c
YiHuan <linyc280@gmai.com>
2024-08-12 11:37:53 +0800

    修該定位

--- ta-project/src/components/card-suffle-button.jsx
+++ ta-project/src/components/card-suffle-button.jsx
@@ -1,5 +1,5 @@
 const CardShuffleButton = ({CardShuffleHandler}) =>{
-  return <button className="float-end z-1 p-2 rounded-md bg-slate-700 text-white my-2 mr-2 hover:bg-slate-400" onClick={()=>CardShuffleHandler()}>ReStart</button>
+  return <button className="absolute float-end z-1 p-2 rounded-md bg-slate-700 text-white my-2 mr-2 hover:bg-slate-400" onClick={()=>CardShuffleHandler()}>ReStart</button>
 }
 
 export { CardShuffleButton }
