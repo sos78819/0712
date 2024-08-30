164b5add468f188c118c8a909043d6c2106fb519
YiHuan <linyc280@gmai.com>
2024-08-30 15:11:00 +0800

    feat:版面調整

--- ta-project/src/components/card-suffle-animate.jsx
+++ ta-project/src/components/card-suffle-animate.jsx
@@ -1,5 +1,5 @@
 const CardSuffleAnimation = ()=>{
-    return <div className="fixed flex items-center justify-center w-full top-0 h-[100vh] z-10 bg-black suffle-animation">
+    return <div className="fixed flex items-center justify-center w-full top-0 h-[100vh]  bg-black suffle-animation">
        <div> <p className="text-white text-xl textBreathe-animation">洗牌中...請靜下心來冥想</p></div>
     </div>
 }
--- ta-project/src/components/card-suffle-button.jsx
+++ ta-project/src/components/card-suffle-button.jsx
@@ -1,6 +1,6 @@
 import { ReSet } from "./ui/reset"
 const CardShuffleButton = ({ CardShuffleHandler }) => {
-  return <button className="bg-white cursor-pointer z-10  rounded-md p-1 mr-1 border-2 border-cyan-600  font-bold " onClick={() => CardShuffleHandler()}>
+  return <button className="bg-white cursor-pointer rounded-md p-1 mr-1 border-2 border-cyan-600  font-bold " onClick={() => CardShuffleHandler()}>
    再抽一次
   </button>
 }
