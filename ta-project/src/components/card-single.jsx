4a61f4913bb9426cd61c6c14e242239f9b0042d6
YiHuan <linyc280@gmai.com>
2024-09-01 15:23:13 +0800

    feat:調整卡牌間距/調整html

--- ta-project/src/components/card-single.jsx
+++ ta-project/src/components/card-single.jsx
@@ -1,7 +1,7 @@
 
 import { twMerge } from 'tailwind-merge'
 const CardSingle = ({children,onClick,className,id}) => {
-  return (<div onClick={onClick} id={id} className={twMerge('bg-black  border-2 border-white rounded-md relative',className)}>
+  return (<div onClick={onClick} id={id} className={twMerge('bg-black mb-1  border-2 border-white rounded-md relative',className)}>
     {children}
   </div>)
 
--- ta-project/src/components/question-form/question-type-form.jsx
+++ ta-project/src/components/question-form/question-type-form.jsx
@@ -10,11 +10,11 @@ const QuestionType = ({ step, typeChange, typeOption, stephandler }) => {
         <div className="w-full h-[100vh] flex justify-center items-center group font-bold">
             <div className="w-[60%] md:w-[40%] relative">
                 <div className="w-full flex pb-5">
-                    <div className="xs:w-10 sm:w-12 group-hover:rotate-12">
-                        <img className="w-full border-sky-100 border-2 rounded-md" src="/cards/TarDefault.jpg" alt="" />
+                    <div className="xs:w-9 sm:w-8 md:w-12 group-hover:rotate-12">
+                        <img className="w-full border-sky-100 border-2 rounded-md shadow-xl shadow-slate-600" src="/cards/TarDefault.jpg" alt="" />
                     </div>
                     <div>
-                        <h1 className=" text-3xl text-justify md:text-7xl ml-2 text-center [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] font-bold">
+                        <h1 className=" text-3xl text-justify md:text-7xl ml-3 text-center [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] font-bold">
                             Tarot online
                         </h1>
                     </div>
